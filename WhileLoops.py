e82753762d75250ce32180b36986d6d33e26e47f
Jardz04 <jc2257@kent.ac.uk>
2024-10-26 18:17:23 +0000

    Assignment 2

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,17 @@
-### Part Two -- your code goes here. 
+import random
+correct_number = random.randint(1,100)
+guess = None
+print("Hello, welcome to the Guessing number game.")
+print("I've picked a number between 1 and 100. Lets see if you can figure it out.")
+while guess != correct_number:
+    try:
+        guess = int(input("Enter your guess: "))
+        if guess < correct_number:
+            print("Too low! Try again.")
+        elif guess > correct_number:
+            print("Too high! Try again.")
+        else:
+            print("Well done! You guessed the correct number.")
+    except ValueError:
+        print("Please enter a valid integer.")
+        print("Thanks for playing!")
--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,3 @@
-### Part One -- your code goes here. 
+for number in range(1,11):
+    square = number ** 2
+    print(f"The square of {number} is {square}")
